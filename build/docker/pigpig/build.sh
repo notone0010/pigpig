bd91691b64bfedad4d321ad22ddeb35cb862ed44
吕松科 <lvsongke@192.168.1.100>
2022-06-30 13:17:16 +0800

    fix(build/docker/pigpig/): fix docker images building

--- build/docker/pigpig/build.sh
+++ build/docker/pigpig/build.sh
@@ -6,6 +6,10 @@
 
 
 mkdir -p "$DST_DIR/scripts/install"
+mkdir -p "$DST_DIR/cert"
 
 cp -rv scripts/lib "$DST_DIR/scripts/"
 cp -v scripts/install/{common.sh,environment.sh} "$DST_DIR/scripts/install"
+
+cp -v configs/cert/{pigpig.crt,pigpig.key} "$DST_DIR/cert"
+cp -v configs/default.yaml "$DST_DIR/pigpig.yaml"
